af9cc64926adc794db2de10390380e4d026156a8
LManX <luke@macrienterprises.com>
2016-11-05 21:09:12 -0400

    Initial Commit

---
+++ TestTextApplication/RAKE.py
@@ -0,0 +1,90 @@
+﻿from __future__ import division
+import operator
+import nltk
+import string
+
+#Taken from http://sujitpal.blogspot.com/2013/03/implementing-rake-algorithm-with-nltk.html
+
+#Functions
+#punct will be 1 char and listed
+def isPunct(word):
+  return len(word) == 1 and word in string.punctuation
+
+#Number will be able to be a float or an int.
+def isNumeric(word):
+  try:
+    float(word) if '.' in word else int(word)
+    return True
+  except ValueError:
+    return False
+
+class RAKE(object):
+
+    def __init__(self):
+        self.stopwords = set(nltk.corpus.stopwords.words())
+        self.top_fraction = 1 # consider top third candidate keywords by score
+
+    def extract(self, text, incl_scores=False):
+        sentences = nltk.sent_tokenize(text) #tokenize by sentence
+        phrase_list = self._generate_candidate_keywords(sentences) #grab candidates by phrase
+        word_scores = self._calculate_word_scores(phrase_list) #score words
+        phrase_scores = self._calculate_phrase_scores(phrase_list, word_scores) #score phrases
+        sorted_phrase_scores = sorted(phrase_scores.items(), key=operator.itemgetter(1), reverse=True) #sort scored phrases in decending order
+        n_phrases = len(sorted_phrase_scores) #length of the sorted list
+        if incl_scores:
+          return sorted_phrase_scores[0:int(n_phrases/self.top_fraction)] #return top third scored candidates
+        else:
+          return map(lambda x: x[0], sorted_phrase_scores[0:int(n_phrases/self.top_fraction)]) #lambda loops through list, returning first element of each?
+
+    def _generate_candidate_keywords(self, sentences): 
+        phrase_list = []
+        for sentence in sentences:
+          words = map(lambda x: "|" if x in self.stopwords else x, nltk.word_tokenize(sentence.lower())) #tokenize sentence by word, taking out stopwords
+          phrase = []
+          for word in words:
+            if word == "|" or isPunct(word): #checks for stopword symbol or puncuation
+              if len(phrase) > 0:
+                phrase_list.append(phrase) #keeps track of words between stopwords and puncuation, 
+                phrase = []
+            else:
+              phrase.append(word)
+        return phrase_list
+
+    def _calculate_word_scores(self, phrase_list):
+        word_freq = nltk.FreqDist()
+        word_degree = nltk.FreqDist()
+        for phrase in phrase_list:
+          degree = len(list(filter(lambda x: not isNumeric(x), phrase))) - 1
+          for word in phrase:
+            word_freq[word] += 1
+            word_degree[word] += degree # other words
+        for word in word_freq.keys():
+          word_degree[word] = word_degree[word] + word_freq[word] # itself
+        # word score = deg(w) / freq(w)
+        word_scores = {}
+        for word in word_freq.keys():
+          word_scores[word] = word_degree[word] / word_freq[word]
+        return word_scores
+
+    def _calculate_phrase_scores(self, phrase_list, word_scores):
+        phrase_scores = {}
+        for phrase in phrase_list:
+          phrase_score = 0
+          for word in phrase:
+            phrase_score += word_scores[word]
+          phrase_scores[" ".join(phrase)] = phrase_score
+        return phrase_scores
+
+    #score semantic similarities between keyphrases, high scoring phrases should describe topic.
+
+    #score sentences by occurance of topic-focused keyphrases, sort, extract highest scoring third of sentences for the summary.
+
+    #Would be nice if could detect "Who" "What" "When" "Where" "How" of a news story. Perhaps a supervised template approach would be easiest?
+
+
+    def test(text):
+      rakeObj = RAKE()
+      #keywords = rakeObj.extract(str(text.encode('ascii','ignore').decode('unicode_escape')), incl_scores=True)
+      keywords = rakeObj.extract(text, incl_scores=True)
+      print (keywords)
+      
---
+++ TestTextApplication/TestTextApplicationBackup.py
@@ -0,0 +1,101 @@
+﻿import sys
+from xml.etree.ElementTree import XML
+import zipfile
+from tkinter import *
+from tkinter.filedialog import *
+from os.path import splitext
+from string import punctuation
+import nltk
+from RAKE import *
+
+
+filename = None
+
+WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+PARA = WORD_NAMESPACE + 'p'
+TEXT = WORD_NAMESPACE + 't'
+
+#method to wipe 
+def newFile():
+    global filename
+    filename = "Untitled"
+    text.delete(0.0,END)
+
+def saveFile():
+    global filename
+    if filename == None:
+        filename = "Untitled"
+
+    savename = filename
+    savename += ".txt"
+    t = text.get(0.0,END)
+    f = open(savename, "w")
+    f.write(t)
+    f.close()
+
+def saveAs():
+    f = asksaveasfile(mode="w", defaultextension=".txt")
+    t = text.get(0.0,END)
+    try:
+        f.write(t.rstrip())#try to write the text to the file, and remove whitespace after the last char
+    except:
+        showerror(title="Oops!", message="Unable to save file...")
+
+def openFile():
+    f = askopenfile(mode="r", filetypes=[("Text Files", '.txt'), ("Office Open XML", '.docx')])
+    if f != None:
+        ext = splitext(f.name)[1]
+        if ext == ".txt":
+            print(splitext(f.name,)[1])
+            t = f.read()
+            text.delete(0.0,END)
+            text.insert(0.0,t)
+        elif ext == ".docx":
+            doc = zipfile.ZipFile(f.name)
+            xml_Content = doc.read('word/document.xml')
+            doc.close()
+            tree = XML(xml_Content)
+
+            paragraphs = []
+            for paragraph in tree.getiterator(PARA):
+                texts = [node.text
+                         for node in paragraph.getiterator(TEXT)
+                            if node.text]
+                if texts:
+                    paragraphs.append(''.join(texts))
+
+            text.delete(0.0,END)
+            text.insert(0.0,'\n\n'.join(paragraphs))
+        else:
+            print("Oops! not a supported file type.")
+
+def TestRake():
+    RAKE.test()
+
+
+mGui = Tk() #make an instance of TK
+mGui.title("My Python Text Editor/Analyzer")
+mGui.minsize(width=400, height = 400)
+mGui.maxsize(width=400, height = 400)
+
+text = Text(mGui)
+text.pack()
+
+menubar = Menu(mGui)
+mGui.config(menu=menubar)
+
+filemenu = Menu(menubar)
+
+filemenu.add_command(label="New", command=newFile)
+filemenu.add_command(label="Save", command=saveFile)
+filemenu.add_command(label="Save As...", command=saveAs)
+filemenu.add_command(label="Open", command=openFile)
+filemenu.add_separator()
+filemenu.add_command(label="Test RAKE", command=TestRake)
+filemenu.add_separator()
+filemenu.add_command(label="Quit", command=mGui.quit)
+
+menubar.add_cascade(label="File", menu=filemenu)
+
+
+mGui.mainloop()
